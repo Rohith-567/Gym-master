8ade3dd87435e61727c85383ebe007b746bea6d2
Rohith-567 <bunnynampelly@gmail.com>
2024-07-13 20:53:52 +0530

    Add files via upload

---
+++ src/components/RewriteSection.jsx
@@ -0,0 +1,23 @@
+
+
+const RewriteSection = () => {
+  return (
+    <div className="capitalize flex flex-col md:flex-row justify-between my-8 px-2 md:px-4 py-6">
+      <div className="flex-1">
+      <img src="https://img.freepik.com/free-photo/young-sportive-woman-doing-exercises-with-jumping-rope-white_176420-7781.jpg?size=626&ext=jpg&uid=R123667877&ga=GA1.1.719611258.1704690894&semt=sph" alt="gym" className="rounded-md"/>
+      </div>
+      <div className="flex-1 flex flex-col justify-between md:px-4 pb-6 md:p-0 px-2 mb-4 md:mb-0">
+      <h1 className="text-lg md:text-5xl font-bold md:leading-snug md:mb-6">craft your physique, rewrite<br /><span className="border-b-4 border-blue-500 py-1">your</span> story</h1>
+      <p className="md:text-xl md:leading-normal mt-4 md:pr-4">Are you ready to take control of your health and transform your life? There's no better time than now to start your fitness journey. Stepping into the gym is not just about building muscles or shedding pounds; it's about building a stronger, more resilient version of yourself.</p>
+
+      <div className="flex justify-between items-center p-2 mt-2 shrink">
+      <div><p className="text-lg"><span className="font-bold text-2xl mx-2">90+</span><br />countries used master</p></div>
+      <div className="mx-2 px-2 md:px-6 border-x-4 border-dashed border-orange-400"><p className="text-lg"><span className="font-bold text-2xl mx-2">180k</span><br />weekly users</p></div>
+      <div><p className="text-lg"><span className="font-bold text-2xl mx-2">2 million+</span><br />membership managed</p></div>
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default RewriteSection
